6806566437c7b9603e9c2cd7d03081c99f70e45d
NadezhdaLobacheva <nadyushland@gmail.com>
2025-09-26 10:17:15 +0300

    Test

---
+++ view.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+class FlashCardView {
+  static async getUserInfo(themes) {
+  }
+
+  static async greetings() {
+    console.log('Привет');
+  }
+}
+
+module.exports = FlashCardView;
